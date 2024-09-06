9b403109546b3d1c3bdee5c8b331a59af28d6080
rGleveau <remy.gleveau@kairios.fr>
2024-09-06 20:11:13 +0200

    finalisation designe

--- components/PresentationPage.vue
+++ components/PresentationPage.vue
@@ -37,11 +37,11 @@
             </div>
         </div>
         <div class="section" id="section2">
-            <div class="cadre-dons col-md-9 col-sm-9">
+            <div class="cadre-dons col-md-9 col-12">
                 <div class="col-md-6 col-sm-6 text-dons-haut">
                         <span>Finance the development of the application</span>
                 </div>
-                <div class="col-md-10 col-sm-10 cadre-don-centre">
+                <div class="col-md-10 col-10 cadre-don-centre">
                     <div class="col-md-5 col-sm-5 montant-don">
                         <span>1000€</span>
                     </div>
@@ -57,13 +57,13 @@
                     <button>Faire une donnation</button>
                 </div>
             </div>
-            <div class="flex-center col-md-12 col-sm-12">
-                <div class="col-md-12 col-sm-10 cadre-info">
-                    <div class="col-md-12 col-sm-3 container-info" >
+            <div class="flex-center col-md-12 col-12">
+                <div class="col-md-12 col-12 cadre-info">
+                    <div class="col-md-12 col-12 container-info" >
                         <div>
-                            <div class="col-md-12 col-sm-12 infos sticky-element" id="about" ref="stickyElement" :class="{ fixed: isSticky }">
+                            <div class="col-md-12 col-12 infos sticky-element" id="about" ref="stickyElement">
                                 <div class="about-container"> 
-                                    <div>
+                                    <div style="text-align: center;">
                                         <img src="aboutIcone.gif">
                                     </div>
                                     <div>
@@ -93,9 +93,9 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-12 col-sm-3 container-info" >
+                    <div class="col-md-12 col-12 container-info" >
                         <div>
-                            <div class="col-md-12 col-sm-12 infos" id="product">
+                            <div class="col-md-12 col-12 infos" id="product">
                                 <div class="product-container">
                                     <div class="product-text">
                                         <h3>Product</h3>
@@ -118,7 +118,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-12 col-sm-3 container-info" >
+                    <div class="col-md-12 col-12 container-info" >
                         <div>
                             <div class="col-md-12 col-sm-12 infos" id="mobile">
                                 <div class="mobile-container">
@@ -127,12 +127,12 @@
                                     </div>
 
                                     <div class="mobile-text">
-                                       <h3>Application Mobile</h3>
-                                       <div>
+                                        <div>
+                                           <h3>Application Mobile</h3>
                                            <p>
                                              Organisez votre budget et vos virement en ouvrant un comptes avec votre IBAN bancaire. Envoyez de l'argent en quelques clic.
                                          </p>
-                                         <a>Télécharge L’APP</a>
+                                         <a href="#">Télécharge L’APP</a>
                                        </div>
                                       
                                     </div>
@@ -141,7 +141,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-12 col-sm-3 container-info">
+                    <div class="col-md-12 col-12 container-info">
                         <div>
                             <div class="col-md-12 col-sm-12 infos" id="security">
                                 <div class="security-img"> 
@@ -192,27 +192,69 @@
                         </div>
                     </div>
 
-                    <div class="col-md-12 col-sm-3 container-info">
+                    <div class="col-md-12 col-12 container-info">
                         <div>
                             <div class="col-md-12 col-sm-12 infos" id="support">
                                 <div class="support-container">
                                     <div class="support-text">
-                                        <h3>Support</h3>
-                                        <p>Contrairement à d’autres application comme { Plum, Bankin , Cashbee , Moka, Yeeld, etc…} qui sont plus oriente à des conseils financier auprès de leurs clientèles.</p>
-                                        <p>Veron n ’a pas pour but tout cela nous analysons pas les rentrée d’argent de nos clients, l’appli offre pas de conseils pour aider l’utilisateurs à mieux gérer son épargne comme les application cité.</p>
-                                        <p>Veron est littéralement un portefeuille comme celle que vous porté dans votre poche,En seulement numérique, vous pouvait l’utilisez quand cela vous semble.</p>
+                                        <div>
+                                            <h3>Support</h3>
+                                            <p>
+                                                Contrairement à d’autres application comme : 
+                                                (Plum, Bankin, Cashbee, Moka, Yeeld, etc…) qui sont plus oriente à des conseils financiers auprès de leurs clientèles. 
+                                                Veron n’analysons pas vous rentrée d’argent.
+                                            </p>
+                                            <p>
+                                                L’application n’offre pas de conseils pour aider les utilisateurs à mieux gérer son épargne comme les applications citées si dessus.
+                                                Elle est littéralement un portefeuille comme celle que vous porté dans votre poche,
+                                            </p>
+                                            <p>
+                                                En seulement numérique, vous pouvait l’utilisez quand cela vous semble
+                                            </p>
+    
+                                            <a>Télécharge L’APP</a>
+                                        </div>
                                     </div>
                                     <div class="support-img"> 
-                                        <img src="suportIcone.png">
+                                        <div class="suppor-img-mobile">
+                                            <img src="supportIcone.png">
+                                            <div class="support-img-appli">
+                                                <img src="googleIcone.png" class="googleIcone">
+                                                <img src="appleIcone.png">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    <div class="col-md-12 col-12 container-info">
+                    <div>
+                        <div class="col-md-12 col-sm-12 infos" id="veron-balance">
+                            <div class="veron-container">
+                                <!-- Section pour l'image du PC -->
+                                <div class="veron-pc">
+                                    <img src="flexibleIcone.png" alt="Veron Dashboard PC Image">
+                                </div>
+
+                                <!-- Section pour le texte à côté du PC -->
+                                <div class="veron-text">
+                                    <h3>Flexible et transparent</h3>
+                                    <p><strong>sans abonnement et sans engagement.</strong></p>
+                                    <p>Le portefeuille numérique qui vous aide dans vos économies d'épargne.</p>
+                                    <p>Avec 0 % de frais de garde et seulement 1.18 % de commission pour toute alimentation de compte.</p>
+                                    <p>Et avec 0 % de commission pour les retraits sur l'application Veron.</p>
+                                    <a href="#">TÉLÉCHARGE L'APP</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
 
-            <div class="partner-container col-md-12 col-sm-12">
+            <div class="partner-container col-md-12 col-12">
                 <h2>Partner</h2>
                 <div class="partner">
                     <div class="quonto">
@@ -229,11 +271,21 @@
             </div>
         </div>
         <div class="section" id="section1Bas">
-            <div class="flex col-md-12">
-                <div class="col-md-4 flex-veron">
-                    <a href="#hautPage">Ver<img src="cadena.png">n</a>
+            <div class="grid-container">
+                <div class="flex col-md-12 col-sm-12">
+                    <div class="col-md-4 col-sm-4 flex-veron">
+                        <p><a href="#hautPage">Ver<img src="cadena.png">n</a></p>
+                    </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 col-sm-4 button-container">
+                        <div class="flex-button">
+                            <button @click="modalInscription()" class="noir">Sign Up</button>
+                            <button @click="modalConnexion()">Login</button>
+                        </div>
+                    </div>
+            </div>
+            <div class="navBar col-md-12 col-sm-12">
+                <div class="col-md-4 col-sm-4 marginTop">
                     <ul class="flex-ul">
                         <li><a href="#About">About</a></li>
                         <li><a href="#Product">Product</a></li>
@@ -241,14 +293,8 @@
                         <li><a href="#Security">Security</a></li>
                     </ul>
                 </div>
-
-                <div class="col-md-4">
-                    <div class="flex-button">
-                        <button class="noir">Sign Up</button>
-                        <button>Login</button>
-                    </div>
-                </div>
             </div>
+        
             <div class="button-app-footer col-md-12" > 
                 <div class="mentionLegal col-md-6">
                     <div style="margin: 30px 0px;">   
@@ -276,6 +322,7 @@
 import modalConnexion from './modalConnexion.vue'
 import modalInscription from './modalInscription.vue'
 import '@/assets/css/smartphone.css'
+import '@/assets/css/general.css'
 import '@/assets/css/pc.css'
 export default {
     components: { modalConnexion, modalInscription},
@@ -304,33 +351,12 @@ export default {
         },
         faq() {
             this.$router.push('faq')
-        },
-        handleScroll() {
-            // this.isSticky = window.scrollY > this.stickyPosition;
-        },
-    },
-    mounted() {
-    this.stickyPosition = this.$refs.stickyElement.offsetTop;
-    window.addEventListener('scroll', this.handleScroll);
-  },
-  beforeDestroy() {
-    window.removeEventListener('scroll', this.handleScroll);
-  },
+        }
+    }
 }
 </script>
 
 
 <style>
-.sticky-element {
-  padding: 10px;
-  transition: all 0.3s ease-in-out;
-}
-
-.sticky-element.fixed {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-}
 
 </style>
